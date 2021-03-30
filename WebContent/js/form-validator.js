17a5227a527f04a629bc79a4ab6247ea25283f37
jossrc <kuro.rc10@gmail.com>
2021-03-30 13:14:40 -0500

    Creando archivo para validar los formularios

---
+++ WebContent/js/form-validator.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+  let forms = document.querySelectorAll('.needs-validation');
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      'submit',
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
