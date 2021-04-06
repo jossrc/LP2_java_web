325de7a124a5b2daf33a6873180b146b5df5e810
jossrc <kuro.rc10@gmail.com>
2021-04-05 19:44:18 -0500

    Creando la Gestion de Usuarios

---
+++ src/mantenimientos/GestionUsuario.java
@@ -0,0 +1,19 @@
+package mantenimientos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import interfaces.UsuarioInterface;
+import model.Usuario;
+import utils.MySQLConexion8;
+
+public class GestionUsuario implements UsuarioInterface {
+
+	@Override
+	public int registrar(Usuario usuario) {
+		int rs = 0;
+
+		return rs;
+	}
+
+}
