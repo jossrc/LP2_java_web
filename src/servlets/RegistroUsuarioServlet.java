493879f631c6d4ace7e805d3e4b52cc2621d720a
jossrc <kuro.rc10@gmail.com>
2021-04-06 10:38:07 -0500

    Usando el nuevo constructor y aplicando formato

--- src/servlets/RegistroUsuarioServlet.java
+++ src/servlets/RegistroUsuarioServlet.java
@@ -12,37 +12,37 @@ import model.Usuario;
 
 @WebServlet(name = "regusu", urlPatterns = { "/regusu" })
 public class RegistroUsuarioServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		System.out.println("Ingresó al Servlet RegistroUsuario");
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Ingresó al Servlet RegistroUsuario");
 
-		String mensajeRegistro = "";
-		String url;
+        String mensajeRegistro = "";
+        String url;
 
-		String nombre = request.getParameter("txtNombre");
-		String apellido = request.getParameter("txtApellido");
-		String usuario = request.getParameter("txtUsuario");
-		String password = request.getParameter("txtPassword");
-		String fechNac = request.getParameter("txtFechNac");
+        String nombre = request.getParameter("txtNombre");
+        String apellido = request.getParameter("txtApellido");
+        String usuario = request.getParameter("txtUsuario");
+        String password = request.getParameter("txtPassword");
+        String fechNac = request.getParameter("txtFechNac");
 
-		Usuario usuarioBD = new Usuario(0, nombre, apellido, usuario, password, fechNac, 1, 1);
+        Usuario usuarioBD = new Usuario(nombre, apellido, usuario, password, fechNac);
 
-		GestionUsuario gestionUsuario = new GestionUsuario();
-		int ok = gestionUsuario.registrar(usuarioBD);
+        GestionUsuario gestionUsuario = new GestionUsuario();
+        int ok = gestionUsuario.registrar(usuarioBD);
 
-		if (ok == 0) {
-			mensajeRegistro += "Error al registrar los datos, revisar";
-			url = "/registro.jsp";
-		} else {
-			mensajeRegistro += "Nuevo usuario agregado, inicie sesión";
-			url = "/login.jsp";
-		}
+        if (ok == 0) {
+            mensajeRegistro += "Error al registrar los datos, revisar";
+            url = "/registro.jsp";
+        } else {
+            mensajeRegistro += "Nuevo usuario agregado, inicie sesión";
+            url = "/login.jsp";
+        }
 
-		request.setAttribute("mensajeRegistro", mensajeRegistro);
-		request.getRequestDispatcher(url).forward(request, response);
+        request.setAttribute("mensajeRegistro", mensajeRegistro);
+        request.getRequestDispatcher(url).forward(request, response);
 
-	}
+    }
 
 }
