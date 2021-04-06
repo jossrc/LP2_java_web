a250b3a26cfdb2013b86ef82486d9e74afa6520d
jossrc <kuro.rc10@gmail.com>
2021-04-06 11:03:01 -0500

    Aplicando formato al GestionUsuario

--- src/mantenimientos/GestionUsuario.java
+++ src/mantenimientos/GestionUsuario.java
@@ -9,32 +9,32 @@ import utils.MySQLConexion8;
 
 public class GestionUsuario implements UsuarioInterface {
 
-	@Override
-	public int registrar(Usuario usuario) {
-		int rs = 0;
+    @Override
+    public int registrar(Usuario usuario) {
+        int rs = 0;
 
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
 
-		try {
-			connection = MySQLConexion8.getConexion();
-			String sql = "insert into tb_usuarios values (null, ?,?,?,?,?, default, default)";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, usuario.getNombre());
-			preparedStatement.setString(2, usuario.getApellido());
-			preparedStatement.setString(3, usuario.getUsuario());
-			preparedStatement.setString(4, usuario.getPassword());
-			preparedStatement.setString(5, usuario.getFechNac());
+        try {
+            connection = MySQLConexion8.getConexion();
+            String sql = "insert into tb_usuarios values (null, ?,?,?,?,?, default, default)";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, usuario.getNombre());
+            preparedStatement.setString(2, usuario.getApellido());
+            preparedStatement.setString(3, usuario.getUsuario());
+            preparedStatement.setString(4, usuario.getPassword());
+            preparedStatement.setString(5, usuario.getFechNac());
 
-			rs = preparedStatement.executeUpdate();
+            rs = preparedStatement.executeUpdate();
 
-		} catch (Exception e) {
-			System.out.println("Error al registrar " + e.getMessage());
-		} finally {
-			MySQLConexion8.closeConexion(connection);
-		}
+        } catch (Exception e) {
+            System.out.println("Error al registrar " + e.getMessage());
+        } finally {
+            MySQLConexion8.closeConexion(connection);
+        }
 
-		return rs;
-	}
+        return rs;
+    }
 
 }
