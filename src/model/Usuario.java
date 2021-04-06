dcb51715bb4f7661baa80903bb3ee5167dbe6c31
jossrc <kuro.rc10@gmail.com>
2021-04-06 10:29:10 -0500

    Agregando nuevo constructor y aplicando formato

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -2,95 +2,105 @@ package model;
 
 public class Usuario {
 
-	private int codigo;
-	private String nombre;
-	private String apellido;
-	private String usuario;
-	private String password;
-	private String fechNac;
-	private int tipo;
-	private int estado;
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String password;
+    private String fechNac;
+    private int tipo;
+    private int estado;
 
-	public Usuario(int codigo, String nombre, String apellido, String usuario, String password, String fechNac,
-			int tipo, int estado) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.usuario = usuario;
-		this.password = password;
-		this.fechNac = fechNac;
-		this.tipo = tipo;
-		this.estado = estado;
-	}
+    public Usuario(int codigo, String nombre, String apellido, String usuario, String password, String fechNac,
+            int tipo, int estado) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.password = password;
+        this.fechNac = fechNac;
+        this.tipo = tipo;
+        this.estado = estado;
+    }
 
-	@Override
-	public String toString() {
-		return "Usuario [codigo=" + codigo + ", nombre=" + nombre + ", apellido=" + apellido + ", usuario=" + usuario
-				+ ", password=" + password + ", fechNac=" + fechNac + ", tipo=" + tipo + ", estado=" + estado + "]";
-	}
+    public Usuario(String nombre, String apellido, String usuario, String password, String fechNac) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.password = password;
+        this.fechNac = fechNac;
+        this.tipo = 2;
+        this.estado = 1;
+    }
 
-	public int getCodigo() {
-		return codigo;
-	}
+    @Override
+    public String toString() {
+        return "Usuario [codigo=" + codigo + ", nombre=" + nombre + ", apellido=" + apellido + ", usuario=" + usuario
+               + ", password=" + password + ", fechNac=" + fechNac + ", tipo=" + tipo + ", estado=" + estado + "]";
+    }
 
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
+    public int getCodigo() {
+        return codigo;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public String getApellido() {
+        return apellido;
+    }
 
-	public String getUsuario() {
-		return usuario;
-	}
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
+    public String getUsuario() {
+        return usuario;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getFechNac() {
-		return fechNac;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setFechNac(String fechNac) {
-		this.fechNac = fechNac;
-	}
+    public String getFechNac() {
+        return fechNac;
+    }
 
-	public int getTipo() {
-		return tipo;
-	}
+    public void setFechNac(String fechNac) {
+        this.fechNac = fechNac;
+    }
 
-	public void setTipo(int tipo) {
-		this.tipo = tipo;
-	}
+    public int getTipo() {
+        return tipo;
+    }
 
-	public int getEstado() {
-		return estado;
-	}
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
 
-	public void setEstado(int estado) {
-		this.estado = estado;
-	}
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
 
 }
