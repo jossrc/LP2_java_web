a2c47f6742a2f7daeadacccac42b0c3e9f0615db
jossrc <kuro.rc10@gmail.com>
2021-04-06 11:00:28 -0500

    Aplicando formato

--- src/utils/MySQLConexion8.java
+++ src/utils/MySQLConexion8.java
@@ -6,35 +6,39 @@ import java.sql.SQLException;
 
 public class MySQLConexion8 {
 
-	public static Connection getConexion() {
-		Connection connection = null;
+    public static Connection getConexion() {
+        Connection connection = null;
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/ciberfarmaweb?useSSL=false&useTimezone=true&serverTimezone=UTC";
-			String user = "root";
-			String password = "mysql";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            int PORT = 3306;
+            String DB_NAME = "ciberfarmaweb";
 
-			connection = DriverManager.getConnection(url, user, password);
+            String URL = "jdbc:mysql://localhost:" + PORT + "/" + DB_NAME
+                    + "?useSSL=false&useTimezone=true&serverTimezone=UTC";
+            String USER = "root";
+            String PASSWORD = "mysql";
 
-		} catch (ClassNotFoundException ex) {
-			System.out.println("Error >> Driver no Instalado CNFE : " + ex);
-		} catch (SQLException ex) {
-			System.out.println("Error >> Driver no Instalado SQLE : " + ex.getMessage());
-		} catch (Exception ex) {
-			System.out.println("Error >> general : " + ex.getMessage());
-		}
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
 
-		return connection;
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error >> Driver no Instalado CNFE : " + ex);
+        } catch (SQLException ex) {
+            System.out.println("Error >> Driver no Instalado SQLE : " + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Error >> general : " + ex.getMessage());
+        }
 
-	}
+        return connection;
 
-	public static void closeConexion(Connection connection) {
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			System.out.println("Problemas al cerrar la conexion");
-		}
-	}
+    }
+
+    public static void closeConexion(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Problemas al cerrar la conexion");
+        }
+    }
 
 }
