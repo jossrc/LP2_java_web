962895c065d6e729811a1cc38f525acf5f692a2e
jossrc <kuro.rc10@gmail.com>
2021-04-06 10:48:45 -0500

    Aplicando formato al LoginServlet

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -9,27 +9,27 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ingreso", urlPatterns = { "/ingreso" })
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		System.out.println("Hola Mundo desde un servlet");
-		String url = "";
-		
-		String usuario = request.getParameter("txtUsuario").trim();
-		String password = request.getParameter("txtPassword").trim();
-		
-		if (usuario.equalsIgnoreCase("admin") && password.equals("12345")) {
-			url = "/principal.jsp";
-			request.setAttribute("saludo", "Bienvenido " + usuario);
-		} else {
-			url = "/login.jsp";
-			request.setAttribute("mensaje", "Usuario o clave incorrecto");
-		}
-		
-		// response.sendRedirect(url);
-		request.getRequestDispatcher(url).forward(request, response);
-		
-	}
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Hola Mundo desde un servlet");
+        String url = "";
+
+        String usuario = request.getParameter("txtUsuario").trim();
+        String password = request.getParameter("txtPassword").trim();
+
+        if (usuario.equalsIgnoreCase("admin") && password.equals("12345")) {
+            url = "/principal.jsp";
+            request.setAttribute("saludo", "Bienvenido " + usuario);
+        } else {
+            url = "/login.jsp";
+            request.setAttribute("mensaje", "Usuario o clave incorrecto");
+        }
+
+        // response.sendRedirect(url);
+        request.getRequestDispatcher(url).forward(request, response);
+
+    }
 
 }
