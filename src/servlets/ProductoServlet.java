4e62d28c4f9f3d7c19714449ae7ca78d24a97107
jossrc <kuro.rc10@gmail.com>
2021-04-19 21:34:28 -0500

    Usando el servlet para el listado de productos

--- src/servlets/ProductoServlet.java
+++ src/servlets/ProductoServlet.java
@@ -1,11 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import mantenimientos.GestionProducto;
+import model.Producto;
 
 @WebServlet(name = "prod", urlPatterns = { "/prod" })
 public class ProductoServlet extends HttpServlet {
@@ -17,22 +22,27 @@ public class ProductoServlet extends HttpServlet {
         System.out.println("Entró al Servlet de Producto");
         String opcion = request.getParameter("opcion");
 
-        switch (opcion) {
-        case "registrar":
-            registrar(request, response);
-            break;
-        case "actualizar":
-            actualizar(request, response);
-            break;
-        case "eliminar":
-            eliminar(request, response);
-            break;
-        case "listado":
-            listar(request, response);
-            break;
-        default:
-            System.out.println("ERROR EN LAS OPCIONES");
-            break;
+        try {
+            switch (opcion) {
+              case "registrar":
+                  registrar(request, response);
+                  break;
+              case "actualizar":
+                  actualizar(request, response);
+                  break;
+              case "eliminar":
+                  eliminar(request, response);
+                  break;
+              case "listado":
+                  listar(request, response);
+                  break;
+              default:
+                  System.out.println("ERROR EN LAS OPCIONES");
+                  break;
+            }
+        } catch (Exception e) {
+            //response.sendRedirect("error.jsp");
+            System.out.println("Error inesperado en el Producto Servlet");
         }
 
     }
@@ -52,9 +62,14 @@ public class ProductoServlet extends HttpServlet {
         System.out.println("Eliminar");
     }
 
-    private void listar(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    private void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Listado");
+
+        // Obtener el listado
+        ArrayList<Producto> lista = new GestionProducto().listado();
+        // Enviar el listado al JSP
+        request.setAttribute("lstProductos", lista);
+        request.getRequestDispatcher("listado_productos.jsp").forward(request, response);
     }
 
 }
