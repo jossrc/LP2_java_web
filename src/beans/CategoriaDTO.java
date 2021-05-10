0a451c629c2ceb187c3ff064fb8399c17649f38c
jossrc <kuro.rc10@gmail.com>
2021-05-10 15:28:51 -0500

    Creando el bean Categoria

---
+++ src/beans/CategoriaDTO.java
@@ -0,0 +1,29 @@
+package beans;
+
+public class CategoriaDTO {
+
+    private int id;
+    private String descripcion;
+
+    public CategoriaDTO(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
