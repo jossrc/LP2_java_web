05e99a6549f1c012d1a2213cf9a86fea6c8dd8f9
jossrc <kuro.rc10@gmail.com>
2021-05-17 20:36:35 -0500

    Creando un Tag Cabecera

---
+++ src/etiquetas/CabeceraTag.java
@@ -0,0 +1,33 @@
+package etiquetas;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class CabeceraTag extends TagSupport {
+
+    private static final long serialVersionUID = 1L;
+
+    public int doStartTag() throws JspException {
+
+        JspWriter out = pageContext.getOut();
+
+        try {
+            out.println("<h1>CiberFarma Presente</h1>");
+        } catch (IOException e) {
+            System.out.println("Error en inicio de CabeceraTag : " + e.getMessage());
+        }
+
+        return SKIP_BODY;
+
+    }
+
+    public int doEndTag() throws JspException {
+
+        return EVAL_PAGE;
+
+    }
+
+}
